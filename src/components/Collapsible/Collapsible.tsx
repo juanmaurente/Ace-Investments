0cd075d65a36ac0178dde99e4c428f94d8d1fd3c
Juan <juanmaurente@gmail.com>
2024-10-29 23:04:37 +1100

    Creating a generic collapsible component reading data from json

--- src/components/Collapsible/Collapsible.tsx
+++ src/components/Collapsible/Collapsible.tsx
@@ -1,62 +1,69 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './Collapsible.module.css';
-import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import data from './Collapsible.json';
 import { useState } from 'react';
 
-const Collapsible = () => {
-	const [isExpanded, setIsExpanded] = useState(false);
+interface Props {
+	header: string;
+	items: string[];
+}
 
-	const toggleExpand = () => {
-		setIsExpanded(!isExpanded);
+const Collapsible = () => {
+	const [expandedIndex, setExpandedIndex] = useState<number | null>(null);
+
+	const toggleExpand = (index: number) => {
+		setExpandedIndex(expandedIndex === index ? null : index);
 	};
 
 	return (
-		<div className={styles.collapsible}>
-			<header className={styles.collapsibleHeader}>
-				<h3>Header</h3>
-				<div
-					className={`${styles.iconContainer} ${
-						isExpanded ? styles.iconExpanded : ''
-					}`}
-					onClick={toggleExpand}>
-					<FontAwesomeIcon icon={faChevronDown} />
+		<div>
+			{(data as Props[]).map((section, index) => (
+				<div key={index} className={styles.collapsible}>
+					<header
+						className={styles.collapsibleHeader}
+						onClick={() =>
+							section.items.length > 0 && toggleExpand(index)
+						}>
+						<h3>{section.header}</h3>
+						{section.items.length > 0 && ( // Show chevron only if items exist
+							<div
+								className={`${styles.iconContainer} ${
+									expandedIndex === index
+										? styles.iconExpanded
+										: ''
+								}`}>
+								<FontAwesomeIcon icon={faChevronDown} />
+							</div>
+						)}
+					</header>
+					{section.items.length > 0 && ( // Show content only if items exist
+						<div
+							className={`${styles.collapsibleContent} ${
+								expandedIndex === index
+									? styles.contentExpanded
+									: ''
+							}`}>
+							<div className={styles.subMenuContainer}>
+								<ul>
+									{section.items.map((item, itemIndex) => (
+										<li key={itemIndex}>
+											<span>
+												{String(itemIndex + 1).padStart(
+													2,
+													'0',
+												)}
+											</span>
+											<span>{item}</span>
+										</li>
+									))}
+								</ul>
+							</div>
+						</div>
+					)}
 				</div>
-			</header>
-			<div
-				className={`${styles.collapsibleContent} ${
-					isExpanded ? styles.contentExpanded : ''
-				}`}>
-				<div className={styles.subMenuContainer}>
-					<ul>
-						<li>
-							<span>01</span>
-							<span>How it works</span>
-						</li>
-						<li>
-							<span>02</span>
-							<span>Features</span>
-						</li>
-						<li>
-							<span>03</span>
-							<span>Data privacy</span>
-						</li>
-						<li>
-							<span>04</span>
-							<span>Case studies</span>
-						</li>
-						<li>
-							<span>05</span>
-							<span>Testimonials</span>
-						</li>
-						<li>
-							<span>06</span>
-							<span>Portfolio calculator</span>
-						</li>
-					</ul>
-				</div>
-			</div>
+			))}
 		</div>
 	);
 };
-
 export default Collapsible;
